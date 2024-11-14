f85a6fb6ea42830a7f574af331f127c2fc1ce324
patrykoles <patrykoles76@wp.pl>
2024-11-14 09:55:32 +0100

    commit before roles

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090b6f485e9c445e90cf70fe84b7d855e3c78c2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+571dfa629fc05179670c4e6a56e9366239e330ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
